61759969a45fc47215b989728a1a3ddae86e4d5c
V R, Harikrishnan <harikrishnan.vr@philips.com>
2024-04-18 10:31:59 +0530

    Initial commit

--- brightener.h
+++ brightener.h
@@ -1,17 +1,39 @@
 #pragma once
 #include <cstdint>
+#include <memory>
+#include <iostream>
 
-struct Image {
-	int rows;
-	int columns;
-	uint8_t pixels[1024 * 1024]; // max 1k x 1k image
+struct Image
+{
+	int m_rows;
+	int m_columns;
+	uint8_t* m_pixels; // max 1k x 1k image
+
+	Image() = delete;
+
+	Image(int rows, int columns) : m_rows(rows), m_columns(columns)
+	{
+		std::cout << "Image created" << std::endl;
+		m_pixels = new uint8_t[rows * columns];
+		memset(m_pixels, NULL, rows * columns);
+	}
+
+	~Image()
+	{
+		std::cout << "Image destroyed" << std::endl;
+		delete m_pixels;
+	}
 };
 
-class ImageBrightener {
+class ImageBrightener
+{
 private:
 	Image m_inputImage;
 public:
-	ImageBrightener(Image& inputImage);
+	ImageBrightener(const Image& inputImage);
+	ImageBrightener(Image&& inputImage);
+	ImageBrightener() = default;
+
 	int BrightenWholeImage();
-	Image GetImage();
+	Image GetImage() const;
 };
--- pass-an-image.cpp
+++ pass-an-image.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include "brightener.h"
 
-int main() {
-    Image image;
-    image.rows = 512;
-    image.columns = 512;
+int main()
+{
+    Image image(512, 512);
+
 	std::cout << "Brightening a 512 x 512 image\n";
-    ImageBrightener brightener(image);
+    ImageBrightener brightener(std::move(image));
     int attenuatedCount = brightener.BrightenWholeImage();
+
     std::cout << "Attenuated " << attenuatedCount << " pixels\n";
 }
